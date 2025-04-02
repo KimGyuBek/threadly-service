35928398bf3db215b6ac73ed35b9c55af1ea5dcd
KimGyuBek <rlarbqor00@naver.com>
2025-04-02 14:25:14 +0900

    feat: 실행 클래스 생성

--- threadly-apps/app-api/src/main/java/com/threadly/ThreadlyApplication.java
+++ threadly-apps/app-api/src/main/java/com/threadly/ThreadlyApplication.java
@@ -1,5 +1,13 @@
 package com.threadly;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ThreadlyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThreadlyApplication.class, args);
+  }
 
 }
