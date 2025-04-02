7b503941d45b8e494d9229e28b5b607573ebeecb
KimGyuBek <rlarbqor00@naver.com>
2025-04-02 16:24:23 +0900

    feat: users 테이블 생성

---
+++ threadly-adapters/adapter-persistence/src/main/resources/flyway/V1__create_users_table.sql
@@ -0,0 +1,14 @@
+drop table if exists users;
+create table users
+(
+    user_id     varchar(255) not null,
+    password    varchar(255) not null,
+    email       varchar(255) not null,
+    phone       varchar(50)  not null,
+    user_type   varchar(50)  not null,
+    is_active   bool         not null,
+    created_at  timestamp    not null default current_timestamp,
+    modified_at timestamp    not null default current_timestamp
+);
+
+/*IDX 추가 */
