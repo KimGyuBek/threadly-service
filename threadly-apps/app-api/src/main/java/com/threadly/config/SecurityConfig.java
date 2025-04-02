5832f4d5319d8f5f8f2d743dc973fca97556c9cb
KimGyuBek <rlarbqor00@naver.com>
2025-04-02 15:06:01 +0900

    feat: security 모든 요청 허용

---
+++ threadly-apps/app-api/src/main/java/com/threadly/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.threadly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+    httpSecurity.csrf(AbstractHttpConfigurer::disable);
+    httpSecurity.cors(AbstractHttpConfigurer::disable);
+
+    /*모든 요청 허용*/
+    httpSecurity.authorizeHttpRequests(
+        auth -> auth.anyRequest().permitAll()
+    );
+
+    return httpSecurity.build();
+
+  }
+
+
+}
