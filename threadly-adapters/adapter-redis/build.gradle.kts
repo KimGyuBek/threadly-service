917a702f526b674144eb7b0ae5edf680509eeb95
KimGyuBek <rlarbqor00@naver.com>
2025-04-14 16:57:04 +0900

    feat: Redis 추가

--- threadly-adapters/adapter-redis/build.gradle.kts
+++ threadly-adapters/adapter-redis/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies{
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
 }
