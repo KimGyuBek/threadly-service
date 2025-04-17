2e4c1e4bb37b6002311e0e46bb0a17e9838e4a45
KimGyuBek <rlarbqor00@naver.com>
2025-04-17 17:03:05 +0900

    feat: 회원가입시 이메일 인증 기능 구현

--- threadly-apps/app-api/src/main/java/com/threadly/auth/AuthService.java
+++ threadly-apps/app-api/src/main/java/com/threadly/auth/AuthService.java
@@ -1,10 +1,12 @@
 package com.threadly.auth;
 
 import com.threadly.ErrorCode;
+import com.threadly.auth.token.response.TokenResponse;
+import com.threadly.auth.verification.PasswordVerificationUseCase;
+import com.threadly.auth.verification.response.PasswordVerificationToken;
 import com.threadly.controller.auth.request.UserLoginRequest;
 import com.threadly.exception.authentication.UserAuthenticationException;
 import com.threadly.exception.user.UserException;
-import com.threadly.token.response.TokenResponse;
 import com.threadly.user.FetchUserUseCase;
 import com.threadly.user.response.UserResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +22,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class AuthService implements PasswordVerificationUseCase {
 
   private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
@@ -28,6 +30,45 @@ public class AuthService {
 
   private final JwtTokenProvider jwtTokenProvider;
 
+  @Override
+  public PasswordVerificationToken getPasswordVerificationToken(String userId, String password) {
+
+    try {
+      /*인증용 토큰 생성*/
+      UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+          userId, password);
+
+      /*인증 시도*/
+      Authentication authenticate = authenticationManagerBuilder.getObject()
+          .authenticate(authenticationToken);
+
+      /*토큰 생성*/
+      String tokenResponse = jwtTokenProvider.generateToken(userId);
+
+      /*SecurityContextHolder에 인증 정보 저장*/
+      SecurityContextHolder.getContext().setAuthentication(authenticate);
+
+      return new PasswordVerificationToken(tokenResponse);
+
+      /*UserNameNotFound*/
+      /*BadCredential*/
+    } catch (UserException | UsernameNotFoundException | BadCredentialsException e) {
+      throw new UserAuthenticationException(ErrorCode.USER_AUTHENTICATION_FAILED);
+
+      /*Disabled*/
+    } catch (DisabledException e) {
+      throw new UserAuthenticationException(ErrorCode.INVALID_USER_STATUS);
+
+      /*Locked*/
+    } catch (LockedException e) {
+      throw new UserAuthenticationException(ErrorCode.ACCOUNT_LOCKED);
+
+      /*나머지*/
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      throw new UserAuthenticationException(ErrorCode.AUTHENTICATION_ERROR);
+    }
+  }
 
   /**
    * login
@@ -43,6 +84,11 @@ public class AuthService {
     try {
       /*사용자 조회*/
       UserResponse findUser = fetchUserUseCase.findUserByEmail(email);
+
+      /*email 인증이 되어있는지 검증*/
+      if (!findUser.isEmailVerified()) {
+        throw new UserAuthenticationException(ErrorCode.EMAIL_NOT_VERIFIED);
+      }
 
       String userId = findUser.getUserId();
 
@@ -64,10 +110,16 @@ public class AuthService {
 
       return tokenResponse;
 
+      /*email 인증 실패*/
+    } catch (UserAuthenticationException e) {
+      System.out.println(e.getMessage());
+      throw e;
+
       /*email로 사용자를 찾을 수 없는 경우*/
       /*UserNameNotFound*/
       /*BadCredential*/
     } catch (UserException | UsernameNotFoundException | BadCredentialsException e) {
+      System.out.println(e.getMessage());
       throw new UserAuthenticationException(ErrorCode.USER_AUTHENTICATION_FAILED);
 
       /*Disabled*/
@@ -85,6 +137,5 @@ public class AuthService {
     }
 
   }
-
 
 }
