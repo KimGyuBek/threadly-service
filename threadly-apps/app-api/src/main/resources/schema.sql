6cb2f132fa78a5217e3dfab902312911b85f4721
KimGyuBek <rlarbqor00@naver.com>
2025-05-12 17:23:48 +0900

    fix: TLY-15-사용자 정보 수정 테스트

--- threadly-apps/app-api/src/main/resources/schema.sql
+++ threadly-apps/app-api/src/main/resources/schema.sql
@@ -1,16 +1,28 @@
 drop table if exists users;
 create table users
 (
-    user_id     varchar(255) not null,
-    user_name   varchar(255) not null,
-    password    varchar(255) not null,
-    email       varchar(255) not null,
-    phone       varchar(50)  not null,
-    user_type   varchar(50)  not null,
-    is_active   bool         not null,
-    is_email_verified bool not null default false,
-    user_profile_id varchar(255) ,
-    created_at  timestamp    not null default current_timestamp,
-    modified_at timestamp    not null default current_timestamp
+    user_id           varchar(255) not null,
+    user_name         varchar(255) not null,
+    password          varchar(255) not null,
+    email             varchar(255) not null,
+    phone             varchar(50)  not null,
+    user_type         varchar(50)  not null,
+    is_active         bool         not null,
+    is_email_verified bool         not null default false,
+    user_profile_id   varchar(255),
+    created_at        timestamp    not null default current_timestamp,
+    modified_at       timestamp    not null default current_timestamp
 );
 
+create table user_profile
+(
+    user_profile_id   varchar(50)  not null primary key,
+    nickname          varchar(255) not null,
+    status_message    varchar(255) not null,
+    bio               varchar(255) not null,
+    gender            varchar(50)  not null,
+    profile_type      varchar(50)  not null default 'USER',
+    profile_image_url varchar(255),
+    created_at        timestamp    not null default current_timestamp,
+    modified_at       timestamp    not null default current_timestamp
+);
---
+++ threadly-apps/app-api/src/test/java/com/threadly/controller/user/UserControllerTest.java
@@ -0,0 +1,80 @@
+package com.threadly.controller.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.threadly.BaseApiTest;
+import com.threadly.CommonResponse;
+import com.threadly.auth.token.response.LoginTokenResponse;
+import com.threadly.controller.user.request.UserProfileRequest;
+import com.threadly.user.UserGenderType;
+import com.threadly.user.response.UserProfileApiResponse;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * UserController Test
+ */
+class UserControllerTest extends BaseApiTest {
+
+  @Test
+  public void setUserProfile_shouldCreateOrUpdateProfile_whenAuthenticatedUserRequests() throws Exception {
+    //given
+    /*로그인 요청 전송*/
+    CommonResponse<LoginTokenResponse> loginResponse = sendLoginRequest(
+        USER_EMAIL_VERIFIED, PASSWORD, new TypeReference<CommonResponse<LoginTokenResponse>>() {
+        }, status().isOk()
+    );
+
+    String accessToken = loginResponse.getData().accessToken();
+
+    //when
+    String nickname = "nickname";
+    String statusMessage = "statusMessage";
+    String bio = "bio";
+    UserGenderType gender = UserGenderType.MALE;
+    String profileImageUrl = "";
+    String requestBody = generateRequestBody(
+        new UserProfileRequest(
+            nickname,
+            statusMessage,
+            bio,
+            gender,
+            profileImageUrl
+        )
+    );
+
+    /*프로필 변경 요청 전송*/
+    CommonResponse<UserProfileApiResponse> response = sendPostRequest(
+        requestBody,
+        "/api/users/profile",
+        status().isCreated(),
+        new TypeReference<CommonResponse<UserProfileApiResponse>>() {
+        },
+        Map.of("Authorization", "Bearer " + accessToken)
+    );
+
+    //then
+    /*로그인 응답 검증*/
+    assertAll(
+        () -> assertTrue(loginResponse.isSuccess()),
+        () -> assertNotNull(loginResponse.getData().accessToken())
+    );
+
+    /*프로필 응답 검증*/
+    assertAll(
+        () -> assertTrue(response.isSuccess()),
+        () -> assertThat(response.getData().nickname()).isEqualTo(nickname),
+        () -> assertThat(response.getData().statusMessage()).isEqualTo(statusMessage),
+        () -> assertThat(response.getData().bio()).isEqualTo(bio),
+        () -> assertThat(response.getData().gender()).isEqualTo(gender.name()),
+        () -> assertThat(response.getData().profileImageUrl()).isEqualTo(profileImageUrl)
+    );
+
+  }
+
+}
