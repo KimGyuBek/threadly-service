e88e7c8560d044c97907abf14b7f88d32b1979ca
KimGyuBek <rlarbqor00@naver.com>
2025-06-06 21:34:05 +0900

    feat: TLY-67 댓글 생성 요청 객체에 validate 검증 추가 및 테스트 추가

--- threadly-apps/app-api/src/main/java/com/threadly/post/request/CreatePostCommentRequest.java
+++ threadly-apps/app-api/src/main/java/com/threadly/post/request/CreatePostCommentRequest.java
@@ -1,12 +1,17 @@
 package com.threadly.post.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 /**
  * 게시글 댓글 생성 요청 DTO
+ *
  * @param content
  */
 public record CreatePostCommentRequest(
+    @NotNull
+    @NotBlank
     String content
-
 ) {
 
 }
