290c8e2d7e181ab6d9ffc793378381a59953f062
KimGyuBek <rlarbqor00@naver.com>
2025-07-05 13:43:08 +0900

    feature/tag 기능 테스트

--- infra/build-deploy.sh
+++ infra/build-deploy.sh
@@ -5,8 +5,7 @@ DATE=$(date +%Y%m%d)
 COMMIT=$(git rev-parse --short HEAD)
 IMAGE_TAG="${DATE}-${COMMIT}"
 
-echo "Building image: kimgyubek/threadly-app:$IMAGE_TAG"
+echo "IMAGE_TAG=$IMAGE_TAG"
 echo "$IMAGE_TAG" > image_tag.txt
 
 echo "Pushing to Docker hub.."
-echo "IMAGE_TAG=$IMAGE_TAG"
