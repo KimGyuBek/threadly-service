2f11a8372a18c80cccf6df4f5a401f33aa730cb3
KimGyuBek <rlarbqor00@naver.com>
2025-07-06 17:23:36 +0900

    feature: docker image 정리 스크립트 테스트

---
+++ infra/clean-old-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+LOG_FILE="./clean_old_images.log"
+exec > >(tee -a "LOG_FILE") 2>&1
+
+IMAGE_NAME=$1
+IMAGE_TAG=$2
+
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+log "======Cleaning old Docker images start======"
+log "Target Repository=${IMAGE_NAME}"
+log "Target Tag=${IMAGE_TAG}"
+
+DELETE_TARGETS=$(docker images $IMAGE_NAME --format '{{.Repository}}:{{.Tag}}' | grep -v -e "${IMAGE_NAME}:${IMAGE_TAG}" || true)
+
+if [[ -z "$DELETE_TARGETS" ]]; then
+  log "No old images to delete."
+else
+  echo "$DELETE_TARGETS" | xargs -r docker rmi
+  log "$DELETE_TARGETS Deleted!"
+fi
