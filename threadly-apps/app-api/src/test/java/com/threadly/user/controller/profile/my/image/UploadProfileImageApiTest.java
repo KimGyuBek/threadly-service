a66be6a1b82169734e8ef3d7ee21903368366a46
KimGyuBek <rlarbqor00@naver.com>
2025-07-28 21:38:11 +0900

    feature:TLY-94-사용자 프로필 이미지 임시 업로드 기능 구현 및 테스트

---
+++ threadly-apps/app-api/src/test/java/com/threadly/user/controller/profile/my/image/UploadProfileImageApiTest.java
@@ -0,0 +1,51 @@
+package com.threadly.user.controller.profile.my.image;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.mock.web.MockMultipartFile;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@DisplayName("내 프로필 이미지 업로드 관련 API 테스트")
+public class UploadProfileImageApiTest extends BaseProfileImageApiTest {
+
+  @Order(1)
+  @Nested
+  @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+  @DisplayName("성공")
+  class success {
+
+    @DisplayName("1. ")
+    @Test
+    public void uploadMyProfileImage_shouldSuccess() throws Exception {
+      //given
+
+      String accessToken = getAccessToken(USER_EMAIL);
+
+      MockMultipartFile file = generateImageWithRatio("01.jpeg", "jpeg", 100, 100);
+      //when
+      sendUploadMyProfileImageRequest(accessToken, file, status().isCreated());
+
+      //then
+
+    }
+  }
+
+  @Order(2)
+  @Nested
+  @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+  @DisplayName("실패")
+  class fail {
+
+  }
+}
+
+
+
