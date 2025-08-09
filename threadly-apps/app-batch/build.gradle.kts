226ce8b501ce391c3a8ab09c21e4866bcc3cc213
KimGyuBek <rlarbqor00@naver.com>
2025-08-09 21:46:03 +0900

    feat:TLY-97-Batch Application 구성

--- threadly-apps/app-batch/build.gradle.kts
+++ threadly-apps/app-batch/build.gradle.kts
@@ -1,16 +1,23 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
+    // Spring Batch 핵심만
     implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.apache.commons:commons-csv:1.10.0")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus-simpleclient:1.13.6")
-    implementation("io.prometheus:simpleclient_pushgateway:0.16.0")
-
-
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    
+    // 필요한 경우에만 외부 모듈 추가
+    // implementation(project(":threadly-core:core-domain"))  // 필요시에만
+    // implementation(project(":threadly-commons"))          // 필요시에만
+    
+    // DB 연결
+    runtimeOnly("org.postgresql:postgresql")
+    
+    // 테스트
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
 }
-tasks.getByName<BootJar>("bootJar"){
-    enabled = false
 
-
+tasks.getByName<BootJar>("bootJar") {
+    enabled = true
+    archiveFileName.set("app-batch.jar")
 }
