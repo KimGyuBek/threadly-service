2ebb60795de55d9a7b6f0ceb1a40ceb54e2ea2d7
KimGyuBek <64270206+KimGyuBek@users.noreply.github.com>
2025-11-05 15:10:41 +0900

    Release: 배포 (#85)

--- threadly-core/core-service/src/main/java/com/threadly/core/service/user/UserQueryService.java
+++ threadly-core/core-service/src/main/java/com/threadly/core/service/user/UserQueryService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserQueryQueryService implements UserQueryUseCase {
+public class UserQueryService implements UserQueryUseCase {
 
   private final UserQueryPort userQueryPort;
 
